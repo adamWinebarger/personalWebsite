44409f3ee51edea09d4c52a63eb063fca16f2ce8
adamWinebarger <adam.k.winebarger@gmail.com>
2024-04-15 17:18:10 -0400

    added composer php req for 'web app' deployment with heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '79a0a7e5fe33532596b7bc24d8a791ba5d7d5548',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '79a0a7e5fe33532596b7bc24d8a791ba5d7d5548',
+            'dev_requirement' => false,
+        ),
+    ),
+);
